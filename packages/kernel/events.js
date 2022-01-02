57841d029fb69eff0fd3d379ea69107fd5724ede
lifesheets <koderpro@hotmail.com>
2022-01-03 01:07:35 +0300

    Checking if the character is inside the colshape.

---
+++ packages/kernel/events.js
@@ -0,0 +1,11 @@
+"use strict"; 
+
+// Проверка, находится ли персонаж внутри колшейпа.
+module.exports = { 
+    "playerEnterColshape": (player, colshape) => {
+        if (player.character && colshape.onEnter) colshape.onEnter(player);
+    },
+    "playerExitColshape": (player, colshape) => {
+        if (player.character && colshape.onExit) colshape.onExit(player);
+    },
+};
