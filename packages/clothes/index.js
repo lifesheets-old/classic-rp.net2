069de6202a9421b54986e2dc8a8a502d80829b9b
lifesheets <koderpro@hotmail.com>
2022-01-03 12:31:16 +0300

    Preparing Json files for future data parsing

---
+++ packages/clothes/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Объявляем локальные переменные и подключаем файлы
+let dataBracelet = require('./data/bracelet.json');
+let dataEar = require('./data/ear.json');
+let dataGlasses = require('./data/glasses.json');
+let dataHat = require('./data/hat.json');
+let dataPants = require('./data/pants.json');
+let dataShoe = require('./data/shoe.json');
+let dataTie = require('./data/tie.json');
+let dataTop = require('./data/top.json');
+let dataWatch = require('./data/watch.json');
+
+module.exports = {
+
+}
